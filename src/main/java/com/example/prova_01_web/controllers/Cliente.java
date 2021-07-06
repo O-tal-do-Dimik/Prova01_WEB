7cfc8f6ceaf4d421adff26d872447d151269898f
Dimítrio Krislley <dimik@MacBook-Pro-de-Dimitrio.local>
2021-07-05 23:16:15 -0300

    corrigindo erros

--- src/main/java/com/example/prova_01_web/controllers/Cliente.java
+++ src/main/java/com/example/prova_01_web/controllers/Cliente.java
@@ -3,7 +3,6 @@ package com.example.prova_01_web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -52,53 +51,48 @@ public class Cliente {
         PreparedStatement chamaBanco =(PreparedStatement)  conexao.prepareStatement(CONSULTA);
         ResultSet resultbanco = chamaBanco.executeQuery();
 
-        response.setContentType("text/html");
-        response.getWriter().append("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Title</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "Restaurante Chines Cansado\n" +
-                "\n" +
-                "<table border=\"1\"><tr>\n" +
-                "        <td>Id</td>\n" +
-                "        <td>Produto</td>\n" +
-                "        <td>Preço</td>\n" +
-                "        <td>Descrição</td>\n" +
-                "        <td>Validade</td>\n" +
-                "        <td>Calorias</td>\n" +
-                "        <td>Carrinho</td>\n" +
-                "    </tr>");
-
-        while(resultbanco.next())
-        {
-            response.getWriter().append("    <tr>\n" +
-                    "        <td>"+resultbanco.getInt("ID")+"</td>\n" +
-                    "        <td>"+resultbanco.getString("NAME")+"</td>\n" +
-                    "        <td>"+resultbanco.getDouble("PRECO")+"</td>\n" +
-                    "        <td>"+resultbanco.getString("DESCRICAO")+"</td>\n" +
-                    "        <td>"+resultbanco.getString("VALIDADE")+"</td>\n" +
-                    "        <td>"+resultbanco.getDouble("CALORIAS")+"</td>\n" +
-                    "        <td><form action='/cliente' method='GET' id='add_carrinho_"+resultbanco.getInt("ID")+"'><input type='hidden' name='id' id='id' value='"+resultbanco.getInt("ID")+"'/><a href='#' onClick='document.getElementById(\"add_carrinho_"+resultbanco.getInt("ID")+"\").submit();'>Adicionar</a></form></td>\n" +
-                    "    </tr>");
-
-        }
-
-        response.getWriter().append("</table>\n" +
-                "\n" +
-                "<a href='/verCarrinho'> Ver Carrinho </a>\n" +
-                "</body>\n" +
-                "</html>");
-
+//        response.setContentType("text/html");
+//        response.getWriter().append("<!DOCTYPE html>\n" +
+//                "<html lang=\"en\">\n" +
+//                "<head>\n" +
+//                "    <meta charset=\"UTF-8\">\n" +
+//                "    <title>Title</title>\n" +
+//                "</head>\n" +
+//                "<body>\n" +
+//                "Restaurante Chines Cansado\n" +
+//                "\n" +
+//                "<table border=\"1\"><tr>\n" +
+//                "        <td>Id</td>\n" +
+//                "        <td>Produto</td>\n" +
+//                "        <td>Preço</td>\n" +
+//                "        <td>Descrição</td>\n" +
+//                "        <td>Validade</td>\n" +
+//                "        <td>Calorias</td>\n" +
+//                "        <td>Carrinho</td>\n" +
+//                "    </tr>");
+//
+//        while(resultbanco.next())
+//        {
+//            response.getWriter().append("    <tr>\n" +
+//                    "        <td>"+resultbanco.getInt("ID")+"</td>\n" +
+//                    "        <td>"+resultbanco.getString("NAME")+"</td>\n" +
+//                    "        <td>"+resultbanco.getDouble("PRECO")+"</td>\n" +
+//                    "        <td>"+resultbanco.getString("DESCRICAO")+"</td>\n" +
+//                    "        <td>"+resultbanco.getString("VALIDADE")+"</td>\n" +
+//                    "        <td>"+resultbanco.getDouble("CALORIAS")+"</td>\n" +
+//                    "        <td><form action='/cliente' method='GET' id='add_carrinho_"+resultbanco.getInt("ID")+"'><input type='hidden' name='id' id='id' value='"+resultbanco.getInt("ID")+"'/><a href='#' onClick='document.getElementById(\"add_carrinho_"+resultbanco.getInt("ID")+"\").submit();'>Adicionar</a></form></td>\n" +
+//                    "    </tr>");
+//
+//        }
+//
+//        response.getWriter().append("</table>\n" +
+//                "\n" +
+//                "<a href='/verCarrinho'> Ver Carrinho </a>\n" +
+//                "</body>\n" +
+//                "</html>");
+//
     }
 
-    @PostMapping
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException, ServletException {
-
-
-    }
 
 
 }
